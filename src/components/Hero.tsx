e274740a6c4cfef63b80f9247635bf1b9cb512fd
Aran.M.Kh <mohamad.khoramdel.mk@gmail.com>
2024-08-13 20:12:38 +0330

    dynamic features has been added

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,25 +1,75 @@
-import React, { useState } from 'react'
+import { useEffect, useState } from 'react'
+
 import Navbar from './Navbar'
+
 import { HeadPhoneData } from '../data/MockData'
 import { IHeadphoneData } from '../types & intefaces/Types&Interfaces'
+
 import { FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa'
+import { FaMessage } from 'react-icons/fa6'
+
+import {motion, AnimatePresence, easeInOut} from "framer-motion"
+import { SlideRight } from '../utility/animateSlide'
 function Hero() {
 
     const [activeData, setActiveData] = useState<IHeadphoneData>(HeadPhoneData[0])
+    const [index, setIndex] = useState<number>(0)
 
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setIndex((prevIndex) => (prevIndex + 1) % HeadPhoneData.length)
+        }, 3000)
+        return () => clearInterval(interval)  
+    }, [index])
+
+    useEffect(() => {
+        setActiveData(HeadPhoneData[index])
+    }, [index])
 
   return (
-    <section className='bg-red-400 h-screen'>
+    <motion.section 
+        initial = {{backgroundImage : `radial-gradient(circle, ${activeData.bgColor} 0%, ${activeData.bgColor} 0%)`}}
+        animate = {{backgroundImage : `radial-gradient(circle, ${activeData.bgColor}aa 0%, ${activeData.bgColor} 70%)`}}
+        transition={{duration: 0.8}}
+    >
       <Navbar/>
       
-      <div className='container grid grid-col-1 md:grid-cols-2 h-screen md:h-[800px] relative'>
+      <div className='container grid grid-col-1 md:grid-cols-2 h-screen md:h-[700px] relative'>
+      
+      
       {/* headphones info section */}
-        <div className='flex flex-col text-white justify-center py-14 md:py-0 xl:max-w-[500px] order-2 md:order-1'>
-                <div className='space-y-4 md:space-y-7 text-center md:text-left'>
-                    <h1 className='text-3xl md:text-4xl font-bold'>{activeData.title}</h1>
-                    <p className='text-sm leading-loose text-white/80'>{activeData.subtitle}</p>
-                    <p className='text-3xl md:text-4xl font-bold'>{activeData.price}</p>
+        <div className='flex flex-col text-white font-poppins justify-center py-14 md:py-0 xl:max-w-[500px] order-2 md:order-1'>
+                <div className='space-y-4 flex flex-col md:space-y-7 text-center md:text-left'>
+                    <AnimatePresence mode='wait'>
+                        <motion.h1
+                        key= {activeData.id}
+                        variants= {SlideRight(0.2)}
+                        initial="hidden"
+                        animate="show"
+                        exit= "exit"
+                        className='text-3xl md:text-4xl font-bold'>{activeData.title}</motion.h1>
+                    </AnimatePresence>
+                    <AnimatePresence mode='wait'>
+                    <motion.p
+                        key= {activeData.id}
+                        variants= {SlideRight(0.4)}
+                        initial="hidden"
+                        animate="show"
+                        exit= "exit"
+                        className='text-sm leading-loose text-white/80'>{activeData.subtitle}</motion.p>
+                    </AnimatePresence>
+                    <AnimatePresence mode='wait'>
+                    <motion.p
+                        key= {activeData.id}
+                        variants= {SlideRight(0.4)}
+                        initial="hidden"
+                        animate="show"
+                        exit= "exit"
+                        className='text-3xl md:text-4xl font-bold'>{activeData.price}</motion.p>
+                    </AnimatePresence>
                 </div>
+                
+                
                 {/* social icons sections */}
                 <div className='flex items-center justify-center md:justify-start gap-6 mt-8 text-4xl'>
                     <FaInstagram className='border p-[6px] rounded-full cursor-pointer hover:bg-emerald-500 duration-200' />
@@ -27,13 +77,40 @@ function Hero() {
                     <FaTwitter className='border p-[6px] rounded-full cursor-pointer hover:bg-emerald-500 duration-200'/>
                 </div>
             </div>
+      
+      
       {/* headphones image section */}
-        <div className='order-1 md:order-2 flex flex-col justify-center items-center'>
-            <img src={activeData.imageSrc} alt="{activeData.title}" />
+        <div className='order-1 md:order-2 flex flex-col justify-center items-center relative'>
+            <AnimatePresence mode='wait'>
+            <motion.img
+                key={activeData.id}
+                initial={{opacity: 0, x:100}}
+                animate={{opacity: 1, x:0}}
+                transition={{duration: 0.4, ease: easeInOut, delay: 0}}
+                exit={{opacity: 0, x: -100}}
+                className='w-[300px] md:w-[400px] xl:w-[500px] relative z-10' 
+                src={activeData.imageSrc} 
+                alt="{activeData.title}" />
+            </AnimatePresence>
+            <AnimatePresence mode='wait'>
+                <motion.div
+                    key={activeData.id}
+                    initial={{opacity: 0}}
+                    animate={{opacity: 1}}
+                    transition={{duration: 0.4, ease: easeInOut, delay: 0}}
+                    exit={{opacity: 0}}
+                    className='text-[300px] absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/2 font-poppins font-extrabold text-white/5 z-0'>{activeData.modal}</motion.div>
+            </AnimatePresence>
+        </div>
+        
+        
+        {/*chat icon section */}
+        <div className='absolute bottom-10 right-10 z-[999]'>
+            <FaMessage className='text-2xl text-white cursor-pointer hover:text-emerald-500 duration-200'/>
         </div>
       </div>
     
-    </section>
+    </motion.section>
   )
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,26 +4,26 @@ import { navbarData } from "../data/MockData";
 import { InavbarData } from "../types & intefaces/Types&Interfaces";
 function Navbar() {
   return (
-    <nav className="py-8 xl:px-32 px-10 flex justify-between items-center text-white">
+    <nav className="py-5 xl:px-24 px-10 flex justify-between items-center text-white z-20 relative">
       
-      <div className="flex gap-1 items-center ">
+      <div className="flex gap-1 items-center justify-center">
         
         <FaApple className="text-3xl"/>
-        <h1 className="text-4xl font-semibold ">AirPods Max</h1>
+        <h1 className="text-3xl font-semibold ">AirPods Max</h1>
       
       </div>
       
       <div className="hidden md:block">
 
-        <ul className="flex items-center justify-center gap-7 text-3xl">
+        <ul className="flex items-center justify-center gap-4 text-2xl">
           {navbarData.map((item : InavbarData) => {
             return (
                 <li key={item.id}>
-                    <a className="inline-block text-xl px-3 uppercase" href={item.link}>{item.title}</a>
+                    <a className="inline-block text-base px-3 uppercase hover:text-indigo-900 duration-300" href={item.link}>{item.title}</a>
                 </li>
             )
           })}
-          <button className="text-2xl ps-14">
+          <button className="text-xl ps-14 hover:text-indigo-900 duration-300">
             <FaRegUser />
           </button>
         </ul>
